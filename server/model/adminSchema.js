f2aa5d7a28f5387ac926c61507d791cb782e9ea1
Abhay Hemant Upadhaya <abhayupadhaya829@gmail.com>
2023-10-07 16:32:40 +0530

    first commit

--- server/model/adminSchema.js
+++ server/model/adminSchema.js
@@ -6,7 +6,7 @@ const adminSchema = new mongoose.Schema({
     email: {
         type:String,
         required : true,
-        unique : [true, "Email id already registered"],
+        
        
     },
     password: {
